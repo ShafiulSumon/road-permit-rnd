bffcd1f434f9a070197aa4bc3e4131a13b64ded6
ShafiulSumon <shafiulahcb1007@gmail.com>
2024-07-04 17:44:46 +0600

    [bug fix] pagination object changed

--- utils/pagination.js
+++ utils/pagination.js
@@ -2,12 +2,12 @@ const pagination = (limit, page_record, current_page, total_record) => {
   const total_page = Math.ceil(total_record / limit);
 
   return {
-    total_record,
-    page_record,
-    total_page,
-    current_page,
-    next_page: current_page >= total_page ? null : current_page + 1,
-    prev_page: current_page <= 1 ? null : current_page - 1,
+    totalRecord: total_record,
+    pageRecord: page_record,
+    totalPage: total_page,
+    currentPage: current_page,
+    nextPage: current_page >= total_page ? null : current_page + 1,
+    prevPage: current_page <= 1 ? null : current_page - 1,
   };
 };
 
